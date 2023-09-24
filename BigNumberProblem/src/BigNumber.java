2ed6b0ac61d7fc0337295238541fb69f0054e9fc
MD. Ismail Hosen Ismail James <md.ismailhosenismailjames@gmail.com>
2023-09-24 16:38:23 +0600

    Now inifinity big number can Multiply together

--- BigNumberProblem/src/BigNumber.java
+++ BigNumberProblem/src/BigNumber.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class BigNumber{
     public static void main(String[] args) {
-        String x = "1239745998987528752752758569356965824902759290970779700986";
-        String y = "879085204820146283765283467865827245263482742884018471";
-        System.out.println(bigSum(x, y));
+        String x = "100";
+        String y = "66";
+        System.out.println(bigMultiply(x, y));
     }
 
     public static String bigSum(String x, String  y){
@@ -37,6 +39,35 @@ public class BigNumber{
         
         if (hand != 0) ans = hand + ans;
 
+        return ans;
+    }
+
+    public static String bigMultiply(String x, String y){
+        int length_x = x.length(), length_y = y.length(), single_digit_x, single_digit_y, single_digit_multiply;
+        ArrayList<String> toSumArray = new ArrayList<String>();
+        String addZero = "";
+        
+        for (int i = length_x - 1; i != -1; i--){
+            single_digit_x = x.charAt(i) - '0';
+            String toSum = "";
+            int mod = 0, hand = 0;
+            for (int j = length_y - 1; j != -1; j--){
+                single_digit_y = y.charAt(j) - '0';
+                single_digit_multiply = single_digit_x * single_digit_y + hand;
+                mod = single_digit_multiply % 10;
+                toSum = mod + toSum;
+                hand = single_digit_multiply / 10;
+            }
+            if (hand != 0) toSum = hand + toSum;
+            toSum += addZero;
+            addZero += '0';
+            toSumArray.add(toSum);
+        }
+        
+        String ans = "0";
+        for (String element : toSumArray) {
+            ans = bigSum(ans, element);
+        }
         return ans;
     }
 }
